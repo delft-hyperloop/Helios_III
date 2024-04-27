d80c3feb07d846e3aa53826c737088bc2543c9cd
Zakrok09 <eccyboo@protonmail.com>
2024-04-27 15:33:30 +0200

    feat: abstractions for initiating commands, generic event listener and charts

---
+++ gs/src/lib/components/abstract/Command.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { invoke } from '@tauri-apps/api/tauri';
+    import util from "$lib/util/util";
+    import type {NamedCommand} from "$lib/types";
+
+    export let command: NamedCommand;
+    export const send = async () => {
+        await invoke(command);
+    };
+</script>
+
+<button class="btn" on:click={send}>{util.snakeToCamel(command)}</button>
