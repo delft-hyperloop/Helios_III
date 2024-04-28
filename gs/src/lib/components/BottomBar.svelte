bd5e8674b109f253092938cd871503e1d6e1529a
Zakrok09 <eccyboo@protonmail.com>
2024-04-28 12:07:55 +0200

    feat: basic layout spots and theme

---
+++ gs/src/lib/components/BottomBar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    let time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+
+    onMount(() => {
+        const interval = setInterval(() => {
+            time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    });
+</script>
+
+
+<footer class="text-zinc-100 text-sm bg-primary-600 w-full flex flex-row flex-nowrap justify-end px-4 gap-4
+               border-t border-black">
+    <p>Sponsored by goose?</p>
+    <p class="">{time}</p>
+</footer>
