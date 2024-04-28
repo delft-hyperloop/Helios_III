9cf80a56f709f205abf2e870176f0a2c6d274a83
Zakrok09 <eccyboo@protonmail.com>
2024-04-29 00:22:40 +0200

    feat: layout with draggable panels + fixes

--- gs/src/lib/components/TitleBar.svelte
+++ gs/src/lib/components/TitleBar.svelte
@@ -6,7 +6,7 @@
 <div data-tauri-drag-region class="bg-surface-900 flex justify-between items-center px-1">
     <div class="flex items-center gap-1">
         <img src="./hyperloop_logo.png" class="h-4" alt="">
-        <span class="text-primary-500 text-xs font-bold">DELFT HYPERLOOP</span>
+        <span class="text-primary-500 font-medium text-xs">DELFT HYPERLOOP</span>
     </div>
     <div class="text-white">
         <button on:click={appWindow.minimize} class="text-success-500">
