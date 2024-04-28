15dcf803e53da771a147ad09fef2367ade7741a9
Zakrok09 <eccyboo@protonmail.com>
2024-04-28 12:07:55 +0200

    feat: basic layout spots and theme

--- gs/src/delft_hyperloop_dark.ts
+++ gs/src/delft_hyperloop_dark.ts
@@ -21,27 +21,27 @@ export const delft_hyperloop_dark: CustomThemeConfig = {
 		"--on-surface": "255 255 255",
 		// =~= Theme Colors  =~=
 		// primary | #0FBA81 
-		"--color-primary-50": "219 245 236", // #dbf5ec
-		"--color-primary-100": "207 241 230", // #cff1e6
-		"--color-primary-200": "195 238 224", // #c3eee0
-		"--color-primary-300": "159 227 205", // #9fe3cd
-		"--color-primary-400": "87 207 167", // #57cfa7
-		"--color-primary-500": "15 186 129", // #0FBA81
-		"--color-primary-600": "14 167 116", // #0ea774
-		"--color-primary-700": "11 140 97", // #0b8c61
-		"--color-primary-800": "9 112 77", // #09704d
-		"--color-primary-900": "7 91 63", // #075b3f
+		"--color-primary-50": "239 248 244", // #EFF8F4
+		"--color-primary-100": "204 227 221", // #cff1e6
+		"--color-primary-200": "191 220 213", // #c3eee0
+		"--color-primary-300": "153 199 187", // #9fe3cd
+		"--color-primary-400": "113 173 159", // #57cfa7
+		"--color-primary-500": "77 156 137", // #0FBA81
+		"--color-primary-600": "0 114 86", // #0ea774
+		"--color-primary-700": "7 80 62", // #0b8c61
+		"--color-primary-800": "7 49 39", // #09704d
+		"--color-primary-900": "6 25 20", // #075b3f
 		// secondary | #4F46E5 
-		"--color-secondary-50": "229 227 251", // #e5e3fb
-		"--color-secondary-100": "220 218 250", // #dcdafa
-		"--color-secondary-200": "211 209 249", // #d3d1f9
-		"--color-secondary-300": "185 181 245", // #b9b5f5
-		"--color-secondary-400": "132 126 237", // #847eed
-		"--color-secondary-500": "79 70 229", // #4F46E5
-		"--color-secondary-600": "71 63 206", // #473fce
-		"--color-secondary-700": "59 53 172", // #3b35ac
-		"--color-secondary-800": "47 42 137", // #2f2a89
-		"--color-secondary-900": "39 34 112", // #272270
+		"--color-secondary-50": "239 248 244", // #e5e3fb
+		"--color-secondary-100": "204 227 221", // #dcdafa
+		"--color-secondary-200": "191 220 213", // #d3d1f9
+		"--color-secondary-300": "153 199 187", // #b9b5f5
+		"--color-secondary-400": "77 156 137", // #847eed
+		"--color-secondary-500": "77 156 137", // #4F46E5
+		"--color-secondary-600": "0 114 86", // #473fce
+		"--color-secondary-700": "7 80 62", // #3b35ac
+		"--color-secondary-800": "7 49 39", // #2f2a89
+		"--color-secondary-900": "6 25 20", // #272270
 		// tertiary | #0EA5E9 
 		"--color-tertiary-50": "219 242 252", // #dbf2fc
 		"--color-tertiary-100": "207 237 251", // #cfedfb
@@ -87,15 +87,15 @@ export const delft_hyperloop_dark: CustomThemeConfig = {
 		"--color-error-800": "127 15 71", // #7f0f47
 		"--color-error-900": "104 12 58", // #680c3a
 		// surface | #495a8f 
-		"--color-surface-50": "228 230 238", // #e4e6ee
-		"--color-surface-100": "219 222 233", // #dbdee9
-		"--color-surface-200": "210 214 227", // #d2d6e3
-		"--color-surface-300": "182 189 210", // #b6bdd2
-		"--color-surface-400": "128 140 177", // #808cb1
-		"--color-surface-500": "73 90 143", // #495a8f
-		"--color-surface-600": "66 81 129", // #425181
-		"--color-surface-700": "55 68 107", // #37446b
-		"--color-surface-800": "44 54 86", // #2c3656
-		"--color-surface-900": "36 44 70", // #242c46
+		"--color-surface-50": "239 240 240", // #e4e6ee
+		"--color-surface-100": "226 228 228", // #dbdee9
+		"--color-surface-200": "196 202 202", // #d2d6e3
+		"--color-surface-300": "167 175 175", // #b6bdd2
+		"--color-surface-400": "137 149 149", // #808cb1
+		"--color-surface-500": "109 121 121", // #495a8f
+		"--color-surface-600": "82 91 91", // #425181
+		"--color-surface-700": "44 48 48", // #37446b
+		"--color-surface-800": "28 31 31", // #2c3656
+		"--color-surface-900": "15 16 16", // #242c46
 	}
 }
--- gs/src/routes/+page.svelte
+++ gs/src/routes/+page.svelte
@@ -1,15 +1,26 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+import {AppRail} from "@skeletonlabs/skeleton";
+import Chart from "$lib/components/generic/Chart.svelte";
+import Command from "$lib/components/abstract/Command.svelte";
+</script>
+
+
+<main class="bg-surface-800 flex-grow border-t border-black
+             flex flex-nowrap flex-row">
+    <!-- !IMPORTANT  -->
+    <div class="w-2/3">
+        <Command command="start_north_bridge" />
+        <AppRail>
+
+        </AppRail>
+    </div>
+    <div class="w-1/3 bg-surface-700 border-l border-r border-black p-4">
+        <Chart refreshRate={5} />
+        <Chart refreshRate={100} />
+    </div>
+</main>
+
+<style lang="css">
+
+</style>
