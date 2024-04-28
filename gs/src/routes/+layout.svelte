93414a4cf05b8ff05a14b41a79fc4f390a28a599
Zakrok09 <eccyboo@protonmail.com>
2024-04-28 12:07:55 +0200

    feat: basic layout spots and theme

--- gs/src/routes/+layout.svelte
+++ gs/src/routes/+layout.svelte
@@ -1,5 +1,32 @@
 <script lang="ts">
 	import '../app.postcss';
+
+    import {onDestroy, onMount} from "svelte";
+    import {listen, type UnlistenFn} from "@tauri-apps/api/event";
+    import {north_bridge_payload, south_bridge_payload} from "$lib/stores/data";
+    import BottomBar from "$lib/components/BottomBar.svelte";
+
+    // two main listeners - update north & south bridge stores
+    let unlisten_north: UnlistenFn;
+    let unlisten_south: UnlistenFn;
+    onMount(async () => {
+        unlisten_south = await listen("south_bridge", (event) => {
+            //@ts-ignore
+            south_bridge_payload.set(event.payload);
+        });
+        unlisten_north = await listen("north_bridge", (event) => {
+            //@ts-ignore
+            north_bridge_payload.set(event.payload);
+        });
+    });
+
+    onDestroy(() => {
+        unlisten_south();
+        unlisten_north();
+    });
 </script>
 
-<slot />
+<div class="flex flex-col w-screen h-screen">
+    <slot />
+    <BottomBar />
+</div>
