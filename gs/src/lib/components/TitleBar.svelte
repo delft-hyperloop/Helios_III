0b1cd0ad77e70fcf128331b554df5d8dde98f8ee
Zakrok09 <eccyboo@protonmail.com>
2024-04-28 17:31:26 +0200

    feat: updated basic layout

---
+++ gs/src/lib/components/TitleBar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { appWindow } from '@tauri-apps/api/window'
+    import Icon from '@iconify/svelte'
+</script>
+
+<div data-tauri-drag-region class="bg-surface-900 flex justify-between items-center px-1">
+    <div class="flex items-center gap-1">
+        <img src="./hyperloop_logo.png" class="h-4" alt="">
+        <span class="text-primary-500 text-xs font-bold">DELFT HYPERLOOP</span>
+    </div>
+    <div class="text-white">
+        <button on:click={appWindow.minimize} class="text-success-500">
+            <Icon icon="mdi:window-minimize" />
+        </button>
+        <button on:click={appWindow.maximize} class="text-warning-500">
+            <Icon icon="mdi:window-maximize" />
+        </button>
+        <button on:click={appWindow.close} class="text-error-500">
+            <Icon icon="mdi:window-close" />
+        </button>
+    </div>
+</div>
---
+++ gs/src/lib/panels/DetailsPanel.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import Command from "$lib/components/abstract/Command.svelte";
+
+    import {TabGroup, Tab} from "@skeletonlabs/skeleton";
+
+    let selectedTab:number = 1;
+
+    let tabs = [
+        {name: "Home", value: 1},
+        {name: "Run", value: 2},
+        {name: "IMU", value: 3},
+        {name: "Pressure", value: 4},
+        {name: "Current", value: 5},
+        {name: "Procedures", value: 6}
+    ];
+</script>
+
+<TabGroup>
+    {#each tabs as tab}
+        <Tab bind:group={selectedTab} value={tab.value} name={tab.name}>{tab.name}</Tab>
+    {/each}
+    <svelte:fragment slot="panel">
+        {#if selectedTab === 1}
+            <Command command="start_north_bridge" />
+        {:else if selectedTab === 2}
+            <Command command="start_south_bridge" />
+        {/if}
+    </svelte:fragment>
+</TabGroup>
