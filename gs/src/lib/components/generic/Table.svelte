4009010e22cef73afcecb71e32334203819ca5e9
Zakrok09 <eccyboo@protonmail.com>
2024-04-30 00:06:37 +0200

    fix: table separators

--- gs/src/lib/components/generic/Table.svelte
+++ gs/src/lib/components/generic/Table.svelte
@@ -8,19 +8,19 @@
 
 <table class="text-sm w-full">
     <thead>
-    <tr class="bg-surface-800 text-center p-4">
+    <tr class="bg-surface-800 text-center p-4 border-b border-b-surface-700">
         <th class="p-2 px-8">
             <span class="font-medium">{titles[0]}</span>
         </th>
         <th class="p-2 px-8 w-1/2">
-            <span class="font-medium text-center">{titles[1]}</span>
+            <span class="font-medium">{titles[1]}</span>
         </th>
     </tr>
     </thead>
     <tbody>
-    {#each tableArr as row, i}
-        <tr class="bg-surface-800">
-            <td class="px-4 border-r border-r-primary-600">{row.name}</td>
+    {#each tableArr as row}
+        <tr class="bg-surface-800 border-b border-b-surface-700">
+            <td class="px-4 border-r border-r-primary-700">{row.name}</td>
             <td class="px-4 py-1 font-mono">{row.value.toFixed(2)}</td>
         </tr>
     {/each}
