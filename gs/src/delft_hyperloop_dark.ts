ce714233e472a882d4dbd96ae7ee1a4cb0f9f60f
Zakrok09 <eccyboo@protonmail.com>
2024-04-29 22:05:18 +0200

    feat: exo2 and nunitosans fonts

--- gs/src/delft_hyperloop_dark.ts
+++ gs/src/delft_hyperloop_dark.ts
@@ -4,8 +4,8 @@ export const delft_hyperloop_dark: CustomThemeConfig = {
 	name: 'delft_hyperloop_dark',
 	properties: {
 		// =~= Theme Properties =~=
-		"--theme-font-family-base": "system-ui",
-		"--theme-font-family-heading": "system-ui",
+		"--theme-font-family-base": "NunitoSans sans-serif",
+		"--theme-font-family-heading": "NunitoSans sans-serif",
 		"--theme-font-color-base": "0 0 0",
 		"--theme-font-color-dark": "255 255 255",
 		"--theme-rounded-base": "9999px",
