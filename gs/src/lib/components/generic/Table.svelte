fbabbc580802c87f6387b204f631f47cff935dc0
Zakrok09 <eccyboo@protonmail.com>
2024-04-30 01:13:12 +0200

    fix: table styling fixes

--- gs/src/lib/components/generic/Table.svelte
+++ gs/src/lib/components/generic/Table.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
     import type {TempTableEntry} from "$lib/types";
     export let tableArr: TempTableEntry[] = [];
-
     export let titles = ["Module", "Temp C°"];
+    export let background = "bg-surface-800";
 </script>
 
 
 <table class="text-sm w-full">
     <thead>
-    <tr class="bg-surface-800 text-center p-4 border-b border-b-surface-700">
+    <tr class="{background} text-center p-4 border-b border-b-surface-700">
         <th class="p-2 px-8">
             <span class="font-medium">{titles[0]}</span>
         </th>
@@ -19,7 +19,7 @@
     </thead>
     <tbody>
     {#each tableArr as row}
-        <tr class="bg-surface-800 border-b border-b-surface-700">
+        <tr class="{background} border-b border-b-surface-700">
             <td class="px-4 border-r border-r-primary-700">{row.name}</td>
             <td class="px-4 py-1 font-mono">{row.value.toFixed(2)}</td>
         </tr>
