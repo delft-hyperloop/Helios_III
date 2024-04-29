e4a2ad74563f6c00b01d79d2ce9993a4b33e525d
Zakrok09 <eccyboo@protonmail.com>
2024-04-29 05:21:01 +0200

    feat: basic keybinds

--- gs/src/lib/panels/DetailsPanel.svelte
+++ gs/src/lib/panels/DetailsPanel.svelte
@@ -1,47 +1,53 @@
 <script lang="ts">
-    import Command from "$lib/components/abstract/Command.svelte";
-
-    import {TabGroup, Tab, AppBar, TabAnchor} from "@skeletonlabs/skeleton";
+    import Keydown from "svelte-keydown";
+    import {TabGroup, Tab} from "@skeletonlabs/skeleton";
     import HomeTab from "$lib/panels/tabs/HomeTab.svelte";
     import ProceduresTab from "$lib/panels/tabs/ProceduresTab.svelte";
+    import {detailTabSet} from "$lib/stores/state";
 
-    let selectedTab:number = 7;
 
     let i = 0;
     let tabs = [
-        {name: "Home", value: ++i},
-        {name: "Run", value: ++i},
-        {name: "Location", value: ++i},
-        {name: "IMU", value: ++i},
-        {name: "Pressure", value: ++i},
-        {name: "Current", value: ++i},
-        {name: "Procedures", value: ++i},
-        {name: "Debug", value: ++i}
+        {name: "Home", value: i++},
+        {name: "Run", value: i++},
+        {name: "Location", value: i++},
+        {name: "IMU", value: i++},
+        {name: "Pressure", value: i++},
+        {name: "Current", value: i++},
+        {name: "Procedures", value: i++},
+        {name: "Debug", value: i++}
     ];
+    let combo:string ='';
 </script>
 
-
+<p>{combo}</p>
+<Keydown on:combo={({detail}) => {
+    const num = parseInt(detail, 10);
+    if (!isNaN(num) && num <= tabs.length) {
+        detailTabSet.set(num - 1); // subtract 1 because your tabs are 0-indexed
+    }
+    }}/>
 <TabGroup regionPanel="h-full" padding="px-3 py-3" regionList="bg-surface-700" border="border-b border-surface-900">
     {#each tabs as tab}
-        <Tab bind:group={selectedTab} value={tab.value} name={tab.name}><span>{tab.name}</span></Tab>
+        <Tab bind:group={$detailTabSet} value={tab.value} name={tab.name}><span>{tab.name}</span></Tab>
     {/each}
     <svelte:fragment slot="panel">
         <div class="h-full" style="margin-top: -16px;">
-            {#if selectedTab === 1}
+            {#if $detailTabSet === 0}
                 <HomeTab />
-            {:else if selectedTab === 2}
+            {:else if $detailTabSet === 1}
                 <p>a</p>
-            {:else if selectedTab === 3}
+            {:else if $detailTabSet === 2}
                 <p>a</p>
-            {:else if selectedTab === 4}
+            {:else if $detailTabSet === 3}
                 <p>a</p>
-            {:else if selectedTab === 5}
+            {:else if $detailTabSet === 4}
                 <p>a</p>
-            {:else if selectedTab === 6}
+            {:else if $detailTabSet === 5}
                 <p>a</p>
-            {:else if selectedTab === 7}
+            {:else if $detailTabSet === 6}
                 <ProceduresTab />
-            {:else if selectedTab === 8}
+            {:else if $detailTabSet === 7}
                 <p>Andreas</p>
             {/if}
         </div>
