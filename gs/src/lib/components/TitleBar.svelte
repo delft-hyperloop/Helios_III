b4e8ec0809f5d75fb44e0126570589fe00276b66
Zakrok09 <eccyboo@protonmail.com>
2024-05-01 18:37:06 +0200

    fix: topbar not cursed

--- gs/src/lib/components/TitleBar.svelte
+++ gs/src/lib/components/TitleBar.svelte
@@ -3,19 +3,19 @@
     import Icon from '@iconify/svelte'
 </script>
 
-<div data-tauri-drag-region class="bg-surface-900 flex justify-between items-center px-1">
+<div data-tauri-drag-region class="bg-surface-900 flex justify-between items-center p-1">
     <div class="flex items-center gap-1">
         <enhanced:img src="/static/images/logo-green.png?w=16" alt="Delft logo" />
         <span class="text-primary-500 font-medium text-xs">DELFT HYPERLOOP</span>
     </div>
-    <div class="text-white">
-        <button on:click={appWindow.minimize} class="text-success-500">
+    <div class="text-surface-300">
+        <button on:click={appWindow.minimize} class="rounded-full bg-surface-800 hover:bg-surface-700 p-1">
             <Icon icon="mdi:window-minimize" />
         </button>
-        <button on:click={appWindow.maximize} class="text-warning-500">
+        <button on:click={appWindow.maximize} class="rounded-full bg-surface-800 hover:bg-surface-700 p-1" >
             <Icon icon="mdi:window-maximize" />
         </button>
-        <button on:click={appWindow.close} class="text-error-500">
+        <button on:click={appWindow.close} class="rounded-full bg-surface-800 hover:bg-surface-700 p-1">
             <Icon icon="mdi:window-close" />
         </button>
     </div>
