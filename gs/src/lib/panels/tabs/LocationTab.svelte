62d551fb1c15a5b2c5017c6b34c7977577b28937
Zakrok09 <eccyboo@protonmail.com>
2024-05-03 01:54:47 +0200

    fix: modularize table

--- gs/src/lib/panels/tabs/LocationTab.svelte
+++ gs/src/lib/panels/tabs/LocationTab.svelte
@@ -1,6 +1,24 @@
+<script lang="ts">
+    import {south_bridge_payload, Table, InitialisationForm} from "$lib";
+
+    let tableArr2:any[][];
+    $: tableArr2 = [
+        ["Some", $south_bridge_payload.value],
+        ["Important", $south_bridge_payload.value],
+        ["Value", $south_bridge_payload.value],
+        ["That", $south_bridge_payload.value],
+        ["Could", $south_bridge_payload.value],
+        ["stay", $south_bridge_payload.value],
+        ["as a", $south_bridge_payload.value],
+        ["table entry", $south_bridge_payload.value],
+        ["here", $south_bridge_payload.value]
+    ]
+</script>
+
 <div class="p-4">
     <h2 class="text-xl font-semibold mb-4">Location & IMU</h2>
-    <div class="">
-
+    <div class="grid grid-cols-2 gap-4">
+        <Table tableArr={tableArr2} background="bg-surface-900" titles={["Important", "Variable"]}/>
+        <Table tableArr={tableArr2} background="bg-surface-900" titles={["Important", "Variable"]}/>
     </div>
 </div>
