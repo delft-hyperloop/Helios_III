d7520d25c693a23674a3ad3ceded2df3b1300776
Zakrok09 <eccyboo@protonmail.com>
2024-05-03 01:22:34 +0200

    fix: optimise inputs and make chart auto resize itself

--- gs/src/lib/index.ts
+++ gs/src/lib/index.ts
@@ -1,1 +1,35 @@
-// place files you want to import through the `$lib` alias in this folder.
+// Panels
+import VitalsPanel from "$lib/panels/VitalsPanel.svelte";
+import LogsPanel from "$lib/panels/LogsPanel.svelte";
+import DetailsPanel from "$lib/panels/DetailsPanel.svelte";
+
+// Tabs
+import BatteriesTab from "$lib/panels/tabs/BatteriesTab.svelte";
+import CurrentTab from "$lib/panels/tabs/CurrentTab.svelte";
+import HomeTab from "$lib/panels/tabs/HomeTab.svelte";
+import LocationTab from "$lib/panels/tabs/LocationTab.svelte";
+import ProceduresTab from "$lib/panels/tabs/ProceduresTab.svelte";
+import RunInitTab from "$lib/panels/tabs/RunInitTab.svelte";
+
+// Components
+import Battery from "$lib/components/Battery.svelte";
+import BottomBar from "$lib/components/BottomBar.svelte";
+import FSM from "$lib/components/FSM.svelte";
+import InitialisationForm from "$lib/components/InitialisationForm.svelte";
+import TitleBar from "$lib/components/TitleBar.svelte";
+import Chart from "$lib/components/generic/Chart.svelte";
+import Table from "$lib/components/generic/Table.svelte";
+import Command from "$lib/components/abstract/Command.svelte";
+import EventListener from "$lib/components/abstract/EventListener.svelte";
+
+// Utils
+import {GraphPlot} from "$lib/util/GraphPlot";
+
+import type {Payload, NamedCommand, EventChannel, TempTableEntry, Log} from "$lib/types";
+
+// Stores
+import {south_bridge_payload, north_bridge_payload} from "$lib/stores/data";
+import {detailTabSet, inputSpeed} from "$lib/stores/state";
+
+export {VitalsPanel, LogsPanel, DetailsPanel, BatteriesTab, CurrentTab, HomeTab, LocationTab, ProceduresTab, RunInitTab, Battery, BottomBar, FSM, InitialisationForm, TitleBar, Chart, Table, Command, EventListener, GraphPlot, south_bridge_payload, north_bridge_payload, detailTabSet, inputSpeed}
+export type {Payload, NamedCommand, EventChannel, TempTableEntry, Log}
--- gs/src/lib/panels/tabs/Batteries.svelte
+++
@@ -1,6 +0,0 @@
-<div class="p-4">
-    <h2 class="text-xl font-semibold mb-4">Batteries</h2>
-    <div class="">
-
-    </div>
-</div>
