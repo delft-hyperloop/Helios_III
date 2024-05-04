e24b539960e0640f16a2482496d22b485523ce89
Zakrok09 <eccyboo@protonmail.com>
2024-05-04 14:46:32 +0200

    fix: migration fixes

--- gs/src/lib/panels/tabs/LocationTab.svelte
+++ gs/src/lib/panels/tabs/LocationTab.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
     import Localiser from "$lib/components/Localiser.svelte";
-    import {south_bridge_payload} from "$lib/stores/data";
-    import Pneumatics from "$lib/components/Pneumatics.svelte";
-</script>
-
-<script lang="ts">
-    import {south_bridge_payload, Table, InitialisationForm} from "$lib";
+    import {south_bridge_payload, Table} from "$lib";
 
     let tableArr2:any[][];
     $: tableArr2 = [
@@ -24,13 +19,10 @@
 <div class="p-4">
     <h2 class="text-xl font-semibold mb-4">Location & IMU</h2>
     <div class="flex flex-col gap-4">
-        <Localiser loc={$south_bridge_payload.value} max={10} turning={true}/>
-        <div class="w-7/12">
-            <Pneumatics pressure_left={10 - $south_bridge_payload.value} pressure_right={$south_bridge_payload.value} max_pressure={10}/>
+        <Localiser loc={$south_bridge_payload.value} max={50} turning={true}/>
+        <div class="grid grid-cols-2 gap-4">
+            <Table tableArr={tableArr2} background="bg-surface-900" titles={["Important", "Variable"]}/>
+            <Table tableArr={tableArr2} background="bg-surface-900" titles={["Important", "Variable"]}/>
         </div>
-    </div>
-    <div class="grid grid-cols-2 gap-4">
-        <Table tableArr={tableArr2} background="bg-surface-900" titles={["Important", "Variable"]}/>
-        <Table tableArr={tableArr2} background="bg-surface-900" titles={["Important", "Variable"]}/>
     </div>
 </div>
