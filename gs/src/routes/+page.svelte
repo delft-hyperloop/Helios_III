155675556db428142a597d6c78fe36ffcd5211a8
Zakrok09 <eccyboo@protonmail.com>
2024-05-07 20:15:08 +0200

    feat: scrollable tabs

--- gs/src/routes/+page.svelte
+++ gs/src/routes/+page.svelte
@@ -7,6 +7,7 @@
     import { getToastStore } from '@skeletonlabs/skeleton';
     import {listen, type UnlistenFn} from "@tauri-apps/api/event";
     import {onDestroy, onMount} from "svelte";
+    import {details_pane, vitals_pane} from "$lib";
 
     let unlisten_error: UnlistenFn;
     const toastStore = getToastStore();
@@ -24,18 +25,22 @@
     onDestroy(() => {
         unlisten_error();
     });
+
+    let style: string;
+    $: style = `height: ${$details_pane-5}vh`;
 </script>
 
-
 <main class="w-full flex-grow border-t border-black overflow-auto">
-    <Splitpanes theme="modern-theme" dblClickSplitter={false}>
+    <Splitpanes theme="modern-theme" dblClickSplitter={false} on:resize={(event) => vitals_pane.set(event.detail[0].size)}>
         <Pane maxSize={50} snapSize={10} minSize={2} size={40} class="rounded-md bg-surface-700" >
             <VitalsPanel />
         </Pane>
         <Pane minSize={50} class="rounded-md bg-surface-900">
-            <Splitpanes horizontal={true} theme="modern-theme-logs" dblClickSplitter={false}>
+            <Splitpanes horizontal={true} theme="modern-theme-logs" dblClickSplitter={false} on:resize={(event) => details_pane.set(event.detail[0].size)}>
                 <Pane minSize={50} class="bg-surface-800">
-                    <DetailsPanel />
+                    <div style={style} class="snap-x scroll-px-4 snap-mandatory scroll-smooth overflow-x-auto">
+                        <DetailsPanel />
+                    </div>
                 </Pane>
                 <Pane size={20} snapSize={10} minSize={5} class="bg-surface-800">
                     <LogsPanel />
