fae0f00803aff1b645d2189d432ea9642b070f31
Zakrok09 <eccyboo@protonmail.com>
2024-05-08 00:54:04 +0200

    feat: use tile grid in vitals tab

--- gs/src/lib/panels/VitalsPanel.svelte
+++ gs/src/lib/panels/VitalsPanel.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {Chart, Battery, Table, FSM, south_bridge_payload} from "$lib";
+    import {Chart, Battery, Table, FSM, south_bridge_payload, TileGrid, Tile} from "$lib";
     import {AppBar} from "@skeletonlabs/skeleton";
     import Icon from "@iconify/svelte";
     import {invoke} from "@tauri-apps/api/tauri";
@@ -52,64 +52,69 @@
             </div>
         </div>
     {:else}
-        <div class="w-full p-4 pb-16 h-full flex flex-col gap-2 overflow-y-auto overflow-x-clip text-surface-50">
-            <!--     FSM       -->
-            <div>
-                {#if width > 550}
-                    <FSM size="sm" />
-                {:else}
-                    <FSM size="lg" />
-                {/if}
-            </div>
-            <!--      Under FSM      -->
-            <div class="flex flex-wrap justify-between">
-                <div class="flex gap-4">
-                    <p>Velocity: <span class="font-mono font-medium">{$south_bridge_payload.value}</span></p>
-                    <p>Position: <span class="font-mono font-medium">{$south_bridge_payload.value}</span></p>
-                </div>
-                <div class="flex gap-4">
-                    <div class="flex gap-2">
-                        <span>LV: </span>
-                        <Battery orientation="horizontal" perc={$south_bridge_payload.value} />
-                    </div>
-                    <div class="flex gap-2">
-                        <span>HV: </span>
-                        <Battery orientation="horizontal" perc={$south_bridge_payload.value} />
-                    </div>
-                </div>
-            </div>
-            <!--     TEMPERATURE TABLE      -->
-            <div class="grid {width < 550 ? 'grid-cols-1' : 'grid-cols-2'} gap-2">
-                <Table {tableArr} />
-                <Table titles={["Variable", "Status"]} tableArr={tableArr2} />
-            </div>
-            <!--     OFFSET GRAPHS       -->
-            <div class="flex flex-col gap-2">
-                <div class="grid gap-2 {width < 550 ? 'grid-cols-1' : 'grid-cols-2'}">
-                    <Chart eventChannel="north_bridge" title="Offset horizontal" refreshRate={5} />
-                    <Chart eventChannel="north_bridge" title="Offset top" refreshRate={100} />
-                    <div class="{width < 550 ? 'col-span-1' : 'col-span-2'}">
-                        <Chart title="Velocity" refreshRate={100} />
-                    </div>
-                </div>
-                <div class="flex flex-wrap items-center gap-x-4 {width < 550 ? 'text-sm' : ''}">
-                    <h3 class="text-lg font-medium">Offset data:</h3>
-                    <div class="flex flex-wrap gap-4">
+        <div class="snap-x scroll-px-0.5 snap-mandatory overflow-x-auto h-[90vh]">
+            <TileGrid className="p-4 w-full" columns="1fr 1fr" rows="" >
+                <!--     FSM       -->
+                <Tile bgToken={800} col_span={2} >
+                    {#if width > 550}
+                        <FSM size="sm" />
+                    {:else}
+                        <FSM size="lg" />
+                    {/if}
+                </Tile>
+                <!--      Under FSM      -->
+                <Tile bgToken={700} col_span={2}>
+                    <div class="flex flex-wrap justify-between">
                         <div class="flex gap-4">
-                            <p class="font-mono">X1: {$south_bridge_payload.value}</p>
-                            <p class="font-mono">X2: {$south_bridge_payload.value}</p>
-                            <p class="font-mono">X3: {$south_bridge_payload.value}</p>
-                            <p class="font-mono">X4: {$south_bridge_payload.value}</p>
+                            <p>Velocity: <span class="font-mono font-medium">{$south_bridge_payload.value}</span></p>
+                            <p>Position: <span class="font-mono font-medium">{$south_bridge_payload.value}</span></p>
                         </div>
                         <div class="flex gap-4">
-                            <p class="font-mono">Y1: {$south_bridge_payload.value}</p>
-                            <p class="font-mono">Y2: {$south_bridge_payload.value}</p>
-                            <p class="font-mono">Y3: {$south_bridge_payload.value}</p>
-                            <p class="font-mono">Y4: {$south_bridge_payload.value}</p>
+                            <div class="flex gap-2">
+                                <span>LV: </span>
+                                <Battery orientation="horizontal" perc={$south_bridge_payload.value} />
+                            </div>
+                            <div class="flex gap-2">
+                                <span>HV: </span>
+                                <Battery orientation="horizontal" perc={$south_bridge_payload.value} />
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </Tile>
+                <!--     TEMPERATURE TABLE      -->
+                <Tile padding="pt-2 pb-1" bgToken={800} col_span={width < 550 ? 2 : 1}>
+                    <Table {tableArr} />
+                </Tile>
+                <Tile padding="pt-2 pb-1" bgToken={800} col_span={width < 550 ? 2 : 1}>
+                    <Table titles={["Variable", "Status"]} tableArr={tableArr2} />
+                </Tile>
+                <!--     OFFSET GRAPHS       -->
+                <Tile padding="py-1" bgToken={800} col_span={width < 550 ? 2 : 1}>
+                    <Chart title="Offset horizontal" refreshRate={100} />
+                </Tile>
+                <Tile padding="py-1" bgToken={800} col_span={width < 550 ? 2 : 1} className="h-full w-full">
+                    <Chart title="Offset horizontal" refreshRate={100} />
+                </Tile>
+                <Tile padding="py-2" bgToken={800} col_span={2}>
+                    <Chart title="Velocity" refreshRate={100} />
+                </Tile>
+<!--                <Tile padding="py-1" bgToken={700} heading="Offset data" col_span={2}>-->
+<!--                    <div class="flex flex-wrap gap-4">-->
+<!--                        <div class="flex gap-4">-->
+<!--                            <p class="font-mono">X1: {$south_bridge_payload.value}</p>-->
+<!--                            <p class="font-mono">X2: {$south_bridge_payload.value}</p>-->
+<!--                            <p class="font-mono">X3: {$south_bridge_payload.value}</p>-->
+<!--                            <p class="font-mono">X4: {$south_bridge_payload.value}</p>-->
+<!--                        </div>-->
+<!--                        <div class="flex gap-4">-->
+<!--                            <p class="font-mono">Y1: {$south_bridge_payload.value}</p>-->
+<!--                            <p class="font-mono">Y2: {$south_bridge_payload.value}</p>-->
+<!--                            <p class="font-mono">Y3: {$south_bridge_payload.value}</p>-->
+<!--                            <p class="font-mono">Y4: {$south_bridge_payload.value}</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </Tile>-->
+            </TileGrid>
         </div>
     {/if}
 </div>
