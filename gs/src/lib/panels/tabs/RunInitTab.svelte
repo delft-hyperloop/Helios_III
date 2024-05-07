d1f2c80aa1a7724f78189c6120265feb7ac4d966
Zakrok09 <eccyboo@protonmail.com>
2024-05-07 20:15:08 +0200

    feat: scrollable tabs

--- gs/src/lib/panels/tabs/RunInitTab.svelte
+++ gs/src/lib/panels/tabs/RunInitTab.svelte
@@ -29,9 +29,7 @@
 <div class="p-4 h-full">
     <h2 class="text-xl font-semibold mb-4">Initialization</h2>
     <TileGrid columns="1fr 1fr 1.5fr" rows="auto 2fr">
-        <Tile row_span={2} col_span={1} className="flex flex-col gap-2">
-            <h3 class="col-span-2 font-medium text-lg">Run initialisation</h3>
-            <hr class="col-span-2">
+        <Tile row_span={2} col_span={1} className="flex flex-col gap-2" heading="Run Initialisation">
             <div class="flex justify-between col-span-2">
                 <button class="btn flex-grow bg-surface-700 rounded-none rounded-l-lg p-2" type="button" class:active={selectedMode === 1} on:click={() => selectedMode = Mode.ShortRun}>
                     Short run
@@ -69,9 +67,7 @@
                 </button>
             </div>
         </Tile>
-        <Tile col_span={1} row_span={1} className="grid grid-cols-2 gap-y-2 auto-rows-min">
-            <h3 class="text-lg font-normal col-span-2">Statuses</h3>
-            <hr class="col-span-2">
+        <Tile col_span={1} row_span={1} className="grid grid-cols-2 gap-y-2 auto-rows-min" heading="Statuses" >
             <p>Helios III</p>
             <Status status={$south_bridge_payload.value % 2 === 0} />
             <p>Propulsion:</p>
@@ -87,9 +83,7 @@
             <p>Levi Thermal:</p>
             <Status status={$south_bridge_payload.value % 2 === 1} />
         </Tile>
-        <Tile col_span={1} row_span={1}>
-            <h3 class="text-lg mb-2 font-normal">Data:</h3>
-            <hr>
+        <Tile col_span={1} row_span={1} heading="Data">
             <Table tableArr={tableArr2} background="bg-surface-900" titles={["important", "variable"]}/>
         </Tile>
         <Tile col_span={2}>
