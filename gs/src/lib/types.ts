30c78b4eb45fbef0e85bae18df9be4a3fc03c741
Kiril Panayotov <eccyboo@protonmail.com>
2024-05-14 18:22:38 +0200

    chore: add pupulate datatypes script

--- gs/src/lib/types.ts
+++ gs/src/lib/types.ts
@@ -1,6 +1,9 @@
 /* AUTO GENERATED USING npm run generate:commands */
-export type NamedCommand = "default_command" | "levitate" | "stop_levitating" | "configure" | "start_run" | "emergency_brake" | "shutdown" | "start_hv" | "stop_hv"
+export type NamedCommand = "DefaultCommand" | "Levitate" | "StopLevitating" | "Configure" | "StartRun" | "EmergencyBrake" | "Shutdown" | "StartHV" | "StopHV"
     /* debug commands */ | 'establish_connection' | 'start_north_bridge' | 'start_south_bridge' | 'abort' | 'start_logs' | 'start_errors'
+
+/*AUTO GENERATED USING npm run generate:datatypes */
+export type NamedDatatype = "DefaultDatatype" | "PropulsionTemperature" | "LevitationTemperature" | "BatteryVoltage" | "TotalBatteryVoltage" | "BatteryTemperature" | "BatteryBalance" | "SingleCellVoltage" | "SingleCellTemperature" | "ChargeState" | "BatteryCurrent" | "BrakeTemperature" | "PropulsionSpeed" | "BrakePressure" | "HighVoltage" | "HighVoltageInsulation" | "FSMState" | "FSMEvent" | "PodDropTriggered" | "Localisation" | "GFDIsolation" | "DefaultBMS"
 
 // Not touched by auto-gen
 export type Payload = {
