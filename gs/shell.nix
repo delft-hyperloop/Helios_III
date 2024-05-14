b5030abbea603883aac30950c0e60aafef5f0d9e
Kiril Panayotov <eccyboo@protonmail.com>
2024-05-14 18:22:17 +0200

    chore: add nix shell for ease of use

---
+++ gs/shell.nix
@@ -0,0 +1,35 @@
+let
+  pkgs = import <nixpkgs> { };
+
+  libraries = with pkgs;[
+    webkitgtk
+    gtk3
+    cairo
+    gdk-pixbuf
+    glib
+    dbus
+    openssl_3
+    librsvg
+  ];
+
+  packages = with pkgs; [
+    pkg-config
+    dbus
+    openssl_3
+    glib
+    gtk3
+    libsoup
+    webkitgtk
+    appimagekit
+    librsvg
+  ];
+in
+pkgs.mkShell {
+  buildInputs = packages;
+
+  shellHook =
+    ''
+      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+      export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+    '';
+}
