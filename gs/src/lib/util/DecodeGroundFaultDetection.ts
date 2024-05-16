5f2f6930acb74278d2e4423e17616f3a60e0764e
Kiril Panayotov <eccyboo@protonmail.com>
2024-05-16 17:57:56 +0200

    fix: logs

--- gs/src/lib/util/DecodeGroundFaultDetection.ts
+++ gs/src/lib/util/DecodeGroundFaultDetection.ts
@@ -2,14 +2,14 @@ interface GroundFaultDetection_Voltage {
     voltage: number;
 }
 interface GroundFaultDetection_Isolation {
-    isolation: number;
+    insulation: number;
 }
 
-export function decodeGroundFaultDetection(data: number): GroundFaultDetection_Voltage {
+export function decodeVoltageFaultDetection(data: number): GroundFaultDetection_Voltage {
     let voltage = (data & 0xFFFF000000000000)>>48;
     return { voltage}
 }
-export function decodeGroundFaultDetection(data: number): GroundFaultDetection_Isolation {
-    let isolation = (data & 0x0000FFFF00000000)>>32;
-    return { isolation }
+export function decodeInsulationFaultDetection(data: number): GroundFaultDetection_Isolation {
+    let insulation = (data & 0x0000FFFF00000000)>>32;
+    return { insulation }
 }
