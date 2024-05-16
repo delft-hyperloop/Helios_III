3cf9e9ab22c5bd721573b562226ddf974ee0b749
Kiril Panayotov <eccyboo@protonmail.com>
2024-05-16 21:53:05 +0200

    breakig

--- gs/src/routes/+page.svelte
+++ gs/src/routes/+page.svelte
@@ -9,41 +9,42 @@
     import {onDestroy, onMount} from "svelte";
     import {details_pane, vitals_pane} from "$lib";
 
-    let unlisten_error: UnlistenFn;
-    const toastStore = getToastStore();
+    let unlisten:UnlistenFn
 
+    let info:any = "";
     onMount(async () => {
-        unlisten_error = await listen("error_channel", (event) => {
-            toastStore.trigger({
-                //@ts-ignore
-                message: event.payload.message,
-                background: 'variant-filled-error',
-            });
+        unlisten = await listen("data", (event) => {
+            console.log(event.payload)
+            info = event.payload;
         });
-    });
+    })
 
     onDestroy(() => {
-        unlisten_error();
-    });
+        unlisten();
+    })
 </script>
 
-<main class="w-full flex-grow border-t border-black overflow-auto">
-    <Splitpanes theme="modern-theme" dblClickSplitter={false} on:resize={(event) => vitals_pane.set(event.detail[0].size)}>
-        <Pane maxSize={50} snapSize={10} minSize={2} size={40} class="rounded-md bg-surface-700" >
-            <VitalsPanel />
-        </Pane>
-        <Pane minSize={50} class="rounded-md bg-surface-900">
-            <Splitpanes horizontal={true} theme="modern-theme-logs" dblClickSplitter={false} on:resize={(event) => details_pane.set(event.detail[0].size)}>
-                <Pane minSize={50} class="bg-surface-800">
-                    <DetailsPanel />
-                </Pane>
-                <Pane size={20} snapSize={10} minSize={5} class="bg-surface-800">
-                    <LogsPanel />
-                </Pane>
-            </Splitpanes>
-        </Pane>
-    </Splitpanes>
-</main>
+<p>"aaa"</p>
+<p>{info}</p>
 
-<style global lang="scss">
-</style>
+
+<!--<main class="w-full flex-grow border-t border-black overflow-auto">-->
+<!--    <Splitpanes theme="modern-theme" dblClickSplitter={false} on:resize={(event) => vitals_pane.set(event.detail[0].size)}>-->
+<!--        <Pane maxSize={50} snapSize={10} minSize={2} size={40} class="rounded-md bg-surface-700" >-->
+<!--            <VitalsPanel />-->
+<!--        </Pane>-->
+<!--        <Pane minSize={50} class="rounded-md bg-surface-900">-->
+<!--            <Splitpanes horizontal={true} theme="modern-theme-logs" dblClickSplitter={false} on:resize={(event) => details_pane.set(event.detail[0].size)}>-->
+<!--                <Pane minSize={50} class="bg-surface-800">-->
+<!--                    <DetailsPanel />-->
+<!--                </Pane>-->
+<!--                <Pane size={20} snapSize={10} minSize={5} class="bg-surface-800">-->
+<!--                    <LogsPanel />-->
+<!--                </Pane>-->
+<!--            </Splitpanes>-->
+<!--        </Pane>-->
+<!--    </Splitpanes>-->
+<!--</main>-->
+
+<!--<style global lang="scss">-->
+<!--</style>-->
