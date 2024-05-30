10adce5fb10c8abc03baf421d8466d94059139e2
Kiril Panayotov <eccyboo@protonmail.com>
2024-05-30 03:34:32 +0200

    emerg

--- gs/src/lib/panels/VitalsPanel.svelte
+++ gs/src/lib/panels/VitalsPanel.svelte
@@ -60,7 +60,7 @@
                     message: "Abort operation triggered",
                     background: 'variant-filled-error',
                 });
-            }} send={async () => await invoke("EmergencyBreak")} className="bg-error-500 text-surface-100 btn py-0 border border-error-500 rounded-sm" cmd="abort"/>
+            }} className="bg-error-500 text-surface-100 btn py-0 border border-error-500 rounded-sm" cmd="EmergencyBrake"/>
         </svelte:fragment>
     </AppBar>
 
