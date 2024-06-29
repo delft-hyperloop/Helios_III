e3ca4f1d0ef49310828933c1a28dc494d61c76a8
Francisco Duque de Morais Amaro <f.duquedemoraisamaro@student.tudelft.nl>
2024-06-29 12:21:58 +0200

    feat: parsers for sensors and pressure

--- gs/src/lib/index.ts
+++ gs/src/lib/index.ts
@@ -35,7 +35,7 @@ import SpeedsInput from "$lib/components/SpeedsInput.svelte";
 import {PlotBuffer, StrokePresets} from "$lib/util/PlotBuffer";
 import {GrandDataDistributor} from "$lib/util/GrandDataDistributor";
 import util from "$lib/util/util";
-import {tempParse, voltParse, addEntryToChart, u64ToDouble} from "$lib/util/parsers";
+import {tempParse, voltParse, addEntryToChart, u64ToDouble,pressureParse,sensorParse} from "$lib/util/parsers";
 
 // Types
 import type {NamedCommand, Log, LogType, IntervalFunction} from "$lib/types";
@@ -49,6 +49,8 @@ export const chartDataStore = new Map<string, uPlot.AlignedData>();
 
 // Export all
 export {
+    sensorParse,
+    pressureParse,
     tempParse,
     voltParse,
     addEntryToChart,
