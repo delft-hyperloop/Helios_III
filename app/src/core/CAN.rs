2fd724a827a802c14eacfd0129c3948df715fca1
Andreas Tsatsanis <code@andreats.com>
2024-07-02 11:50:57 +0200

    BMS and ground Fault detection files for messages adn ids

--- app/src/core/CAN.rs
+++
@@ -1,2 +0,0 @@
-//TODO
-// This file is just an idea butttt, we could make the can tasks here or go like fuck it they stay on the fsm peripherals which i also am considering renaming as i really dont like the name //
