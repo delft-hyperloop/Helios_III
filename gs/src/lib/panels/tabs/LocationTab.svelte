131a99b2085f76fa779348054e2ed98bf45fa747
Andreas Tsatsanis <code@andreats.com>
2024-07-02 12:02:55 +0200

    fix: styling of vitals when closed

--- gs/src/lib/panels/tabs/LocationTab.svelte
+++ gs/src/lib/panels/tabs/LocationTab.svelte
@@ -1,24 +1,12 @@
 <script lang="ts">
-    import {south_bridge_payload, Table, InitialisationForm} from "$lib";
+    import Localiser from "$lib/components/Localiser.svelte";
+    import {south_bridge_payload} from "$lib/stores/data";
 
-    let tableArr2:any[][];
-    $: tableArr2 = [
-        ["Some", $south_bridge_payload.value],
-        ["Important", $south_bridge_payload.value],
-        ["Value", $south_bridge_payload.value],
-        ["That", $south_bridge_payload.value],
-        ["Could", $south_bridge_payload.value],
-        ["stay", $south_bridge_payload.value],
-        ["as a", $south_bridge_payload.value],
-        ["table entry", $south_bridge_payload.value],
-        ["here", $south_bridge_payload.value]
-    ]
 </script>
 
 <div class="p-4">
     <h2 class="text-xl font-semibold mb-4">Location & IMU</h2>
-    <div class="grid grid-cols-2 gap-4">
-        <Table tableArr={tableArr2} background="bg-surface-900" titles={["Important", "Variable"]}/>
-        <Table tableArr={tableArr2} background="bg-surface-900" titles={["Important", "Variable"]}/>
+    <div class="">
+        <Localiser loc={$south_bridge_payload.value} max={10} turning={true}/>
     </div>
 </div>
--- gs/src/lib/stores/state.ts
+++ gs/src/lib/stores/state.ts
@@ -1,4 +1,4 @@
 import { writable, type Writable } from 'svelte/store';
 
-export const detailTabSet: Writable<number> = writable(4);
+export const detailTabSet: Writable<number> = writable(2);
 export const inputSpeed: Writable<number> = writable(50);
