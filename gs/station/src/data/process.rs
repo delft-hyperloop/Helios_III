c38b04fe3e66a18aea324afd861e6e39fb33990d
FranciscoAmaro24 <fdmamaro24@gmail.com>
2024-07-17 17:31:11 +0200

    fixed bms

--- gs/station/src/data/process.rs
+++ gs/station/src/data/process.rs
@@ -15,7 +15,7 @@ pub fn process(datapoint: &Datapoint) -> ProcessedData {
     let value = match datapoint.datatype {
         Datatype::ChargeStateLow | Datatype::ChargeStateHigh => x * 0.01,
         Datatype::TotalBatteryVoltageHigh | Datatype::TotalBatteryVoltageLow => x / 100.0 - 2.0,
-        Datatype::BatteryCurrentLow | Datatype::BatteryCurrentHigh=> (x-2000.0)*0.1,
+        Datatype::BatteryCurrentLow | Datatype::BatteryCurrentHigh => (x - 2000.0) * 0.1,
         Datatype::BrakingCommDebug => x * 3.3 / 65535.0,
         Datatype::IMDVoltageDetails => {
             if datapoint.value == 65535 {
@@ -88,11 +88,131 @@ pub fn process(datapoint: &Datapoint) -> ProcessedData {
         | Datatype::Module7MaxVoltage
         | Datatype::Module8MaxVoltage
         | Datatype::BatteryMinVoltageLow
-        | Datatype:: BatteryMaxVoltageHigh
+        | Datatype::BatteryMaxVoltageHigh
         | Datatype::BatteryMinVoltageHigh
         | Datatype::BatteryMaxVoltageLow
         | Datatype::BatteryVoltageHigh
-        | Datatype::BatteryVoltageLow=> {
+        | Datatype::BatteryVoltageLow
+        | Datatype::SingleCellVoltageLV1
+        | Datatype::SingleCellVoltageLV2
+        | Datatype::SingleCellVoltageLV3
+        | Datatype::SingleCellVoltageLV4
+        | Datatype::SingleCellVoltageLV5
+        | Datatype::SingleCellVoltageLV6
+        | Datatype::SingleCellVoltageLV7
+        | Datatype::SingleCellVoltageLV8
+        | Datatype::SingleCellVoltageHV1
+        | Datatype::SingleCellVoltageHV2
+        | Datatype::SingleCellVoltageHV3
+        | Datatype::SingleCellVoltageHV4
+        | Datatype::SingleCellVoltageHV5
+        | Datatype::SingleCellVoltageHV6
+        | Datatype::SingleCellVoltageHV7
+        | Datatype::SingleCellVoltageHV8
+        | Datatype::SingleCellVoltageHV9
+        | Datatype::SingleCellVoltageHV10
+        | Datatype::SingleCellVoltageHV11
+        | Datatype::SingleCellVoltageHV12
+        | Datatype::SingleCellVoltageHV13
+        | Datatype::SingleCellVoltageHV14
+        | Datatype::SingleCellVoltageHV15
+        | Datatype::SingleCellVoltageHV16
+        | Datatype::SingleCellVoltageHV17
+        | Datatype::SingleCellVoltageHV18
+        | Datatype::SingleCellVoltageHV19
+        | Datatype::SingleCellVoltageHV20
+        | Datatype::SingleCellVoltageHV21
+        | Datatype::SingleCellVoltageHV22
+        | Datatype::SingleCellVoltageHV23
+        | Datatype::SingleCellVoltageHV24
+        | Datatype::SingleCellVoltageHV25
+        | Datatype::SingleCellVoltageHV26
+        | Datatype::SingleCellVoltageHV27
+        | Datatype::SingleCellVoltageHV28
+        | Datatype::SingleCellVoltageHV29
+        | Datatype::SingleCellVoltageHV30
+        | Datatype::SingleCellVoltageHV31
+        | Datatype::SingleCellVoltageHV32
+        | Datatype::SingleCellVoltageHV33
+        | Datatype::SingleCellVoltageHV34
+        | Datatype::SingleCellVoltageHV35
+        | Datatype::SingleCellVoltageHV36
+        | Datatype::SingleCellVoltageHV37
+        | Datatype::SingleCellVoltageHV38
+        | Datatype::SingleCellVoltageHV39
+        | Datatype::SingleCellVoltageHV40
+        | Datatype::SingleCellVoltageHV41
+        | Datatype::SingleCellVoltageHV42
+        | Datatype::SingleCellVoltageHV43
+        | Datatype::SingleCellVoltageHV44
+        | Datatype::SingleCellVoltageHV45
+        | Datatype::SingleCellVoltageHV46
+        | Datatype::SingleCellVoltageHV47
+        | Datatype::SingleCellVoltageHV48
+        | Datatype::SingleCellVoltageHV49
+        | Datatype::SingleCellVoltageHV50
+        | Datatype::SingleCellVoltageHV51
+        | Datatype::SingleCellVoltageHV52
+        | Datatype::SingleCellVoltageHV53
+        | Datatype::SingleCellVoltageHV54
+        | Datatype::SingleCellVoltageHV55
+        | Datatype::SingleCellVoltageHV56
+        | Datatype::SingleCellVoltageHV57
+        | Datatype::SingleCellVoltageHV58
+        | Datatype::SingleCellVoltageHV59
+        | Datatype::SingleCellVoltageHV60
+        | Datatype::SingleCellVoltageHV61
+        | Datatype::SingleCellVoltageHV62
+        | Datatype::SingleCellVoltageHV63
+        | Datatype::SingleCellVoltageHV64
+        | Datatype::SingleCellVoltageHV65
+        | Datatype::SingleCellVoltageHV66
+        | Datatype::SingleCellVoltageHV67
+        | Datatype::SingleCellVoltageHV68
+        | Datatype::SingleCellVoltageHV69
+        | Datatype::SingleCellVoltageHV70
+        | Datatype::SingleCellVoltageHV71
+        | Datatype::SingleCellVoltageHV72
+        | Datatype::SingleCellVoltageHV73
+        | Datatype::SingleCellVoltageHV74
+        | Datatype::SingleCellVoltageHV75
+        | Datatype::SingleCellVoltageHV76
+        | Datatype::SingleCellVoltageHV77
+        | Datatype::SingleCellVoltageHV78
+        | Datatype::SingleCellVoltageHV79
+        | Datatype::SingleCellVoltageHV80
+        | Datatype::SingleCellVoltageHV81
+        | Datatype::SingleCellVoltageHV82
+        | Datatype::SingleCellVoltageHV83
+        | Datatype::SingleCellVoltageHV84
+        | Datatype::SingleCellVoltageHV85
+        | Datatype::SingleCellVoltageHV86
+        | Datatype::SingleCellVoltageHV87
+        | Datatype::SingleCellVoltageHV88
+        | Datatype::SingleCellVoltageHV89
+        | Datatype::SingleCellVoltageHV90
+        | Datatype::SingleCellVoltageHV91
+        | Datatype::SingleCellVoltageHV92
+        | Datatype::SingleCellVoltageHV93
+        | Datatype::SingleCellVoltageHV94
+        | Datatype::SingleCellVoltageHV95
+        | Datatype::SingleCellVoltageHV96
+        | Datatype::SingleCellVoltageHV97
+        | Datatype::SingleCellVoltageHV98
+        | Datatype::SingleCellVoltageHV99
+        | Datatype::SingleCellVoltageHV100
+        | Datatype::SingleCellVoltageHV101
+        | Datatype::SingleCellVoltageHV102
+        | Datatype::SingleCellVoltageHV103
+        | Datatype::SingleCellVoltageHV104
+        | Datatype::SingleCellVoltageHV105
+        | Datatype::SingleCellVoltageHV106
+        | Datatype::SingleCellVoltageHV107
+        | Datatype::SingleCellVoltageHV108
+        | Datatype::SingleCellVoltageHV109
+        | Datatype::SingleCellVoltageHV110
+        | Datatype::SingleCellVoltageHV111
+        | Datatype::SingleCellVoltageHV112 => {
             if x == 200.0 {
                 0.0
             } else {
